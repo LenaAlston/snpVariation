d84300314a00094473e86e1c1274a97690c8df2b
LAlston <lena.alston92@gmail.com>
2015-11-17 11:37:25 -0500

    Added upload box.

--- app/js/services.js
+++ app/js/services.js
@@ -2,3 +2,9 @@
 
 /* Services */
 
+var geneVar = angular.module('geneVarApp', []);  // do I need that?
+
+function showname(){
+        var  file = document.fileform.uploadBox.value ;
+        document.fileform.filename.value = file;
+    }
