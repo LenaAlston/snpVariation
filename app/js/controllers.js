1ad065380a1a31c65c56385102721a8ebf46e95c
LAlston <lena.alston92@gmail.com>
2015-11-17 10:50:29 -0500

    Basic web layout.

---
+++ app/js/controllers.js
@@ -0,0 +1,28 @@
+'use strict';
+
+/* Controllers */
+
+var phonecatApp = angular.module('phonecatApp', []);
+
+phonecatApp.controller('PhoneListCtrl', function($scope) {
+  $scope.phones = [
+    {'name': 'Nexus S',
+     'snippet': 'Fast just got faster with Nexus S.'},
+    {'name': 'Motorola XOOM™ with Wi-Fi',
+     'snippet': 'The Next, Next Generation tablet.'},
+    {'name': 'MOTOROLA XOOM™',
+     'snippet': 'The Next, Next Generation tablet.'}
+  ];
+});
+
+phonecatApp.controller('ArticleController', function($scope) {
+  $scope.articles = [
+    {'title': 'Presidential Debate',
+     'snippet': 'Fast just got faster with Nexus S.'},
+    {'title': 'Hillary coming to DC',
+     'snippet': 'The Next, Next Generation tablet.'},
+    {'title': 'Lenas learning Angularjs holllaaaa',
+     'snippet': 'The Next, Next Generation tablet.'}
+  ];
+});
+
